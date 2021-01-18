e1907e1b6ecd4e88086f46fa2510266375a4700b
Anne-Lys-Rene <73004608+Anne-Lys-Rene@users.noreply.github.com>
2021-01-17 20:02:56 -0500

    Add files via upload

---
+++ One-Tree-Hill-trivia/sketch.js
@@ -0,0 +1,11 @@
+var ques1;
+
+function preload(){}
+
+function setup(){
+    ques1 = new question ("When did Haley and Nathan get Married ?", "Senior Year", "After High School", "After Jamie's birth", "First Year of College", 1)
+}
+
+function draw(){
+    ques1.display();
+}
